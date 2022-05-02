9716a47765d25ae0f05798cf79ed9d70509ca95b
Denver McCarthy <dmichaelspdx@gmail.com>
2022-05-02 13:21:43 -0700

    created fetch function and useeffect to set characters in state, added load state

--- src/views/CharacterList/CharacterList.jsx
+++ src/views/CharacterList/CharacterList.jsx
@@ -1,5 +1,16 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { fetchCharacters } from '../../services/rick';
 
 export default function CharacterList() {
+  const [loading, setLoading] = useState(true);
+  const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    const fetch = async () => {
+      const { results } = await fetchCharacters();
+      setCharacters(results);
+      setLoading(false);
+    };
+    fetch();
+  }, []);
   return <div>CharacterList</div>;
 }
