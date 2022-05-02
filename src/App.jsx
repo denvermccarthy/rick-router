32ba5269f4d400f3a64d9cebfb9361ec32448ae3
Denver McCarthy <dmichaelspdx@gmail.com>
2022-05-02 13:15:13 -0700

    added list detail routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
+import { Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import CharacterDetail from './views/CharacterDetail/CharacterDetail';
+import CharacterList from './views/CharacterList/CharacterList';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Rick and Morty Characters</h1>
+      <Switch>
+        <Route path={'/'}>
+          <CharacterList />
+        </Route>
+        <Route path={'/character/:id'}>
+          <CharacterDetail />
+        </Route>
+      </Switch>
+    </>
+  );
 }
